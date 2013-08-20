23f8d2f3ad5d3df95a2913bfbd87425870e9863b
Colin King <colin.king.96@gmail.com>
2013-08-20 00:45:28 -0400

    Attempt one:

--- KyleSeqFinder/src/kyleseqfinder/KyleSeqFinder.java
+++ KyleSeqFinder/src/kyleseqfinder/KyleSeqFinder.java
@@ -1,24 +1,32 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * @author Colin King
+ * @date August 19, 2013
  */
 package kyleseqfinder;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author colin
- */
 public class KyleSeqFinder {
 
-    /**
-     * @param args the command line arguments
-     */
+    //writes the name and sequnece to a specified file
     public static void writeData(File file, String name, String seq) {
+        try {
+            //"true" tells the writer to not overwrite the data in the file each time
+            FileWriter writeFile = new FileWriter(file, true);
+            writeFile.write(">" + name + "\n");
+            writeFile.write(seq + "\n");
+            writeFile.close();
+        } catch (IOException ex) {
+            Logger.getLogger(KyleSeqFinder.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-
+    //returns a list of all the files ending in ".ace" within a specified folder
     public static File[] finder(String dirName) {
         File dir = new File(dirName);
 
@@ -28,7 +36,20 @@ public class KyleSeqFinder {
                 return filename.endsWith(".ace");
             }
         });
+    }
 
+    public static void deleteFolder(File folder) {
+        File[] files = folder.listFiles();
+        if (files != null) { //some JVMs return null for empty dirs
+            for (File f : files) {
+                if (f.isDirectory()) {
+                    deleteFolder(f);
+                } else {
+                    f.delete();
+                }
+            }
+        }
+        folder.delete();
     }
 
     public static void main(String[] args) {
@@ -40,8 +61,35 @@ public class KyleSeqFinder {
          * Take the line with the sequence, write it
          * after finishing a folder, make new folder
          */
-        for (File file: finder("/Users/KyleKing/Desktop/Sequence Files/1000009.2.fasta.assemblies")) {
-            System.out.println(file.toString());
+        String searchFor = "RD HWI-EAS";
+//        String fileExt = ".ace";
+        String folderRoot = "/Users/KyleKing/Desktop/Sequence Files/";
+        //String folderRoot = "/home/colin/SequenceFiles/";
+        String[] folderNames = new String[]{"1000009.2.fasta.assemblies", "1000009.3.fasta.assemblies", "1000009.4.fasta.assemblies", "1000010.4.fasta.assemblies", "1000010.6.fasta.assemblies", "1000010.7.fasta.assemblies", "1000010.8.fasta.assemblies"};
+        //String[] folderNames = new String[]{"ABC123", "ABC456", "ABC789"};
+        //for each folder in the above list
+        for (String folder : folderNames) {
+            folder = folderRoot + folder;
+            //create the new folder, won't create it if it already exists
+            deleteFolder(new File(folder + ".txt"));
+            new File(folder + ".txt").mkdir();
+            for (File file : finder(folder)) {
+                try {
+                    //create a new file, if not already made
+                    File writeToFile = new File(folder + ".txt/" + file.getName() + ".fasta");
+                    //search, and write
+                    Scanner scan = new Scanner(file);
+                    while (scan.hasNext()) {
+                        String returnedText = scan.findInLine(searchFor);
+                        String text = scan.nextLine();
+                        if (returnedText != null) {
+                            writeData(writeToFile, returnedText + text, scan.nextLine());
+                        }
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(KyleSeqFinder.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
     }
 }
